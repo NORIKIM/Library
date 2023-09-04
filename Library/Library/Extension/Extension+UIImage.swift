2977d1ad539cacf1028637e10a65557f29018f25
NORIKIM <jinagold.k@gmail.com>
2023-09-05 04:47:25 +0900

    uiimage resize extenstion 추가

---
+++ Library/Library/Extension/Extension+UIImage.swift
@@ -0,0 +1,22 @@
+//
+//  Extension+UIImage.swift
+//  Library
+//
+//  Created by 김지나 on 2023/09/05.
+//
+
+import UIKit
+
+extension UIImage {
+    func resize(newWidth: CGFloat) -> UIImage {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
+
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { context in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        return renderImage
+    }
+}
