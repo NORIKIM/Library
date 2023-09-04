8d28de3471f9b5d13d01d5a9967ae27a78084bc5
NORIKIM <jinagold.k@gmail.com>
2023-09-04 20:48:19 +0900

    error enum 생성 및 적용

--- Library/Library/Network/Service.swift
+++ Library/Library/Network/Service.swift
@@ -8,22 +8,29 @@
 import Foundation
 import Moya
 
+enum ServiceError: Error {
+    case failDecoding
+    case failRequest
+}
+
 class Service {
     static let shared = Service()
     private let provider = MoyaProvider<Network>()
     
-    func requestSerch(keyword: String, page: Int, completion: @escaping (BookWrapper?)->()) {
+    func requestSerch(keyword: String, page: Int, completion: @escaping (BookWrapper?, ServiceError?)->()) {
         provider.request(.search(keyword: keyword, page: page)) { result in
             switch result {
             case .success(let response):
                 do {
                     let book = try response.map(BookWrapper.self)
-                    completion(book)
+                    completion(book, nil)
                 } catch(let error) {
                     print(error.localizedDescription)
+                    completion(nil, .failDecoding)
                 }
             case .failure(let error) :
                 print(error.localizedDescription)
+                completion(nil, .failRequest)
             }
         }
     }
